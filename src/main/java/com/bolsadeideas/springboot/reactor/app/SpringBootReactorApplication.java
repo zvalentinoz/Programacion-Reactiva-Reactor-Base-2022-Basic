a5a7bd2124c80d0655e50d69297857dd37a92f25
Valentino <valentinoarhuata18@gmail.com>
2025-12-05 00:03:26 -0500

    primerSubidoAntiguo

---
+++ src/main/java/com/bolsadeideas/springboot/reactor/app/SpringBootReactorApplication.java
@@ -0,0 +1,62 @@
+package com.bolsadeideas.springboot.reactor.app;
+
+import java.util.Collection;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.bolsadeideas.springboot.reactor.app.models.Usuario;
+
+import reactor.core.publisher.Flux;
+
+@SpringBootApplication
+public class SpringBootReactorApplication implements CommandLineRunner {
+
+	//importamos un Logger para ver mejor los datos 
+	private static final Logger log = LoggerFactory.getLogger(SpringBootReactorApplication.class);
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootReactorApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Flux<String> nombres = Flux.just("Andres Fultaño","Pedro Torres","Maria esmeralda" , 
+				"Juan pelarez","Marcos kistom ","Ernesto lopez");
+		
+			Flux<Usuario> Usuarios = nombres.map(nomb -> new Usuario(nomb.split(" ")[0].toUpperCase(), nomb.split(" ")[1].toUpperCase()  ))
+				.filter(n -> n.getNombre().toLowerCase().equals("maria") )
+				.doOnNext(usuario -> { 
+					if(usuario == null) {
+						throw new RuntimeException("Nombres no pueden ser vacios");
+					}
+				 System.out.println( usuario.getNombre().concat(" ").concat(usuario.getApellido()) );	 
+	})   
+		.map(usuario2 -> {
+			String nombre = usuario2.getNombre().toLowerCase();
+		  usuario2.setNombre(nombre);
+		  return usuario2;
+	}); 
+	      
+			Usuarios.subscribe(e -> log.info(e.toString()) ,
+				errors -> log.error(errors.getMessage()) ,
+				new Runnable() {
+					
+					@Override
+					public void run() {
+					
+					 log.info("Ha finalizado la ejecucion observable con exito!");	
+					}
+					
+				});
+		
+		
+	}
+	
+	
+	
+}
